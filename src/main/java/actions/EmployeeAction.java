b4d9881beddbfc70b953b4ec622a05bffd6fde82
Ryotaro Shimizu <shimizurr@hydrosoken.co.jp>
2022-09-19 14:00:42 +0900

    従業員Newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -50,4 +50,19 @@ public class EmployeeAction extends ActionBase{
 
     }
 
+    /**
+     * 新規登録画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void entryNew() throws ServletException, IOException {
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView());
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_EMP_NEW);
+
+    }
+
+
 }
